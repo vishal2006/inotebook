6829b2c0548857ea0ffb090ee18742b00fb11893
vishal2006 <90980201+vishal2006@users.noreply.github.com>
2023-07-19 21:03:36 +0530

    commit 3

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -9,7 +9,7 @@ const Signup = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const {name, email, password} = credentials;
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://inotebook-8xct.onrender.com/api/auth/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from './noteContext'
 
 const NoteState = (props) =>{
 
-    const host = "http://localhost:5000"
+    const host = "https://inotebook-8xct.onrender.com"
     const notesInitial = []
     const [notes, setnotes] = useState(notesInitial)
 
